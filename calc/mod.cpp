c077a671d6184197182ad104b76c48b5c65d76eb
yrcho <rogi008@naver.com>
2025-06-08 01:04:13 +0900

    dev/mod

--- calc/mod.cpp
+++ calc/mod.cpp
@@ -1,5 +1,14 @@
 #include "mod.h"
+#include "sub.h"
+
 
 int mod(int x, int y) {
-	return 0;
+	int sum = x;
+	for (int i = 0; i < y; i++) {
+		if (sub(sum, y) < 0) {
+			break;
+		}
+		sum = sub(sum, y);
+	}
+	return sum;
 }
