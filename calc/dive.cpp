c9093bb21a9aaf6cda237486dd8b1ccaecae5b16
yrcho <rogi008@naver.com>
2025-06-08 01:08:22 +0900

    dev/dive

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -1,5 +1,16 @@
 #include "dive.h"
 
+#include "sub.h"
+
 int dive(int x, int y) {
-	return 0;
+	int sum = x;
+	int cnt = 0;
+	for (int i = 0; i < y; i++) {
+		if (sub(sum, y) < 0) {
+			break;
+		}
+		sum = sub(sum, y);
+		cnt++;
+	}
+	return cnt;
 }
